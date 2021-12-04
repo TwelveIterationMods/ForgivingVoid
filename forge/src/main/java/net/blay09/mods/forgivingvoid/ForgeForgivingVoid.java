87e10b39d6cd73b9ee573ef09e9afc96bfefc35b
blay09 <blay09@hotmail.de>
2021-12-04 23:40:58 +0100

    Update to Minecraft 1.18

--- forge/src/main/java/net/blay09/mods/forgivingvoid/ForgeForgivingVoid.java
+++ forge/src/main/java/net/blay09/mods/forgivingvoid/ForgeForgivingVoid.java
@@ -4,7 +4,7 @@ import net.blay09.mods.balm.api.Balm;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(ForgivingVoid.MOD_ID)
 public class ForgeForgivingVoid {
@@ -12,7 +12,7 @@ public class ForgeForgivingVoid {
     public ForgeForgivingVoid() {
         Balm.initialize(ForgivingVoid.MOD_ID, ForgivingVoid::initialize);
 
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
 }

75a460a7eef7b833198f583c14f6b16b255f5954
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2023-09-26 14:42:59 +0200

    feat: Update to Minecraft 1.20.2

--- forge/src/main/java/net/blay09/mods/forgivingvoid/ForgeForgivingVoid.java
+++ forge/src/main/java/net/blay09/mods/forgivingvoid/ForgeForgivingVoid.java
@@ -4,7 +4,6 @@ import net.blay09.mods.balm.api.Balm;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod(ForgivingVoid.MOD_ID)
 public class ForgeForgivingVoid {
@@ -12,7 +11,7 @@ public class ForgeForgivingVoid {
     public ForgeForgivingVoid() {
         Balm.initialize(ForgivingVoid.MOD_ID, ForgivingVoid::initialize);
 
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 
 }

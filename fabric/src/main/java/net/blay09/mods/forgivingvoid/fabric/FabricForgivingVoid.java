3badd71bd2cfe36a2039cec15630555242f578e1
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-05-23 15:06:40 +0200

    feat: Update to Minecraft 1.20.6

--- fabric/src/main/java/net/blay09/mods/forgivingvoid/fabric/FabricForgivingVoid.java
+++ fabric/src/main/java/net/blay09/mods/forgivingvoid/fabric/FabricForgivingVoid.java
@@ -1,12 +1,13 @@
 package net.blay09.mods.forgivingvoid.fabric;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.forgivingvoid.ForgivingVoid;
 import net.fabricmc.api.ModInitializer;
 
 public class FabricForgivingVoid implements ModInitializer {
     @Override
     public void onInitialize() {
-        Balm.initialize(ForgivingVoid.MOD_ID, ForgivingVoid::initialize);
+        Balm.initialize(ForgivingVoid.MOD_ID, EmptyLoadContext.INSTANCE, ForgivingVoid::initialize);
     }
 }
--- neoforge/src/main/java/net/blay09/mods/forgivingvoid/NeoForgeForgivingVoid.java
+++ neoforge/src/main/java/net/blay09/mods/forgivingvoid/NeoForgeForgivingVoid.java
@@ -1,6 +1,9 @@
 package net.blay09.mods.forgivingvoid;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.neoforge.NeoForgeLoadContext;
+import net.neoforged.bus.EventBus;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.IExtensionPoint;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
@@ -8,10 +11,9 @@ import net.neoforged.fml.common.Mod;
 @Mod(ForgivingVoid.MOD_ID)
 public class NeoForgeForgivingVoid {
 
-    public NeoForgeForgivingVoid() {
-        Balm.initialize(ForgivingVoid.MOD_ID, ForgivingVoid::initialize);
-
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
+    public NeoForgeForgivingVoid(IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modEventBus);
+        Balm.initialize(ForgivingVoid.MOD_ID, context, ForgivingVoid::initialize);
     }
 
 }

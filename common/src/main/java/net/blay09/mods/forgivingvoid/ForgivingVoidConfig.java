93680b8e0bd5f5105306c71a350e37a6e730a054
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-17 20:41:00 +0100

    feat: Update to Minecraft 1.21.5

--- common/src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoidConfig.java
+++ common/src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoidConfig.java
@@ -3,12 +3,5 @@ package net.blay09.mods.forgivingvoid;
 import net.blay09.mods.balm.api.Balm;
 
 public class ForgivingVoidConfig {
-    public static ForgivingVoidConfigData getActive() {
-        return Balm.getConfig().getActive(ForgivingVoidConfigData.class);
-    }
-
-    public static void initialize() {
-        Balm.getConfig().registerConfig(ForgivingVoidConfigData.class, null);
-    }
 
 }
--- common/src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoidConfigData.java
+++ common/src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoidConfigData.java
@@ -1,9 +1,9 @@
 package net.blay09.mods.forgivingvoid;
 
-import net.blay09.mods.balm.api.config.BalmConfigData;
-import net.blay09.mods.balm.api.config.Comment;
-import net.blay09.mods.balm.api.config.Config;
-import net.blay09.mods.balm.api.config.ExpectedType;
+import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.api.config.reflection.Comment;
+import net.blay09.mods.balm.api.config.reflection.Config;
+import net.blay09.mods.balm.api.config.reflection.NestedType;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.HashSet;
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 @Config(ForgivingVoid.MOD_ID)
-public class ForgivingVoidConfigData implements BalmConfigData {
+public class ForgivingVoidConfigData {
 
     @Comment("The distance to the minimum y level at which Forgiving Void should forgive the player and send them towards the sky.")
     public int triggerAtDistanceBelow = 32;
@@ -46,20 +46,27 @@ public class ForgivingVoidConfigData implements BalmConfigData {
     @Comment("Set to true to have tridents with loyalty be affected by Forgiving Void. Not supported on Forge.")
     public boolean tridentForgiveness = false;
 
-    @ExpectedType(String.class)
+    @NestedType(String.class)
     @Comment("Effects applied to a player when they fall through the void, in the format \"effect|duration|amplifier\"")
     public List<String> fallThroughVoidEffects = List.of("minecraft:blindness|60|3");
 
     @Comment("List of dimension ids to be allowed for Forgiving Void. Options triggerInOverworld etc. take priority.")
-    @ExpectedType(ResourceLocation.class)
+    @NestedType(ResourceLocation.class)
     public Set<ResourceLocation> dimensionAllowList = new HashSet<>();
 
     @Comment("List of additional dimension ids to be deny-listed from Forgiving Void. Options triggerInOverworld etc. take priority. Ignored if dimensionAllowList is set.")
-    @ExpectedType(ResourceLocation.class)
+    @NestedType(ResourceLocation.class)
     public Set<ResourceLocation> dimensionDenyList = new HashSet<>();
 
     @Comment("List of entity ids to be allowed for Forgiving Void. On Forge this only supports living entities.")
-    @ExpectedType(ResourceLocation.class)
+    @NestedType(ResourceLocation.class)
     public Set<ResourceLocation> entityAllowList = Set.of(ResourceLocation.withDefaultNamespace("player"));
 
+    public static ForgivingVoidConfigData getActive() {
+        return Balm.getConfig().getActiveConfig(ForgivingVoidConfigData.class);
+    }
+
+    public static void initialize() {
+        Balm.getConfig().registerConfig(ForgivingVoidConfigData.class);
+    }
 }

59247c2d52c3a84fd35aea1cf7053ddf4eec3e27
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-06-18 20:44:41 +0200

    feat: Update to Forge 1.21.6

--- forge/src/main/java/net/blay09/mods/forgivingvoid/ForgeForgivingVoid.java
+++ forge/src/main/java/net/blay09/mods/forgivingvoid/ForgeForgivingVoid.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ForgeForgivingVoid {
 
     public ForgeForgivingVoid(FMLJavaModLoadingContext context) {
-        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        final var loadContext = new ForgeLoadContext(context.getModBusGroup());
         Balm.initializeMod(ForgivingVoid.MOD_ID, loadContext, ForgivingVoid::initialize);
 
         context.registerDisplayTest(IExtensionPoint.DisplayTest.IGNORE_ALL_VERSION);

d4a6b9e644b54c116d5f6c3253099a48e192f2c4
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-09-30 13:55:53 +0200

    fix: Prevent Forgiving Void damage when using Elytra #56

--- common/src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoid.java
+++ common/src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoid.java
@@ -128,6 +128,10 @@ public class ForgivingVoid {
     }
 
     private static boolean isOrMayFly(Entity entity) {
+        if (entity instanceof LivingEntity livingEntity && livingEntity.isFallFlying()) {
+            return true;
+        }
+
         if (!(entity instanceof Player player)) {
             return false;
         }

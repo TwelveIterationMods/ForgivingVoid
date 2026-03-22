6b3308b4954ee5d93cd2aa9448f0522ebfeff0ee
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 18:56:45 +0100

    feat: Update to Minecraft 26.1

--- neoforge/src/main/java/net/blay09/mods/forgivingvoid/NeoForgeForgivingVoid.java
+++ neoforge/src/main/java/net/blay09/mods/forgivingvoid/NeoForgeForgivingVoid.java
@@ -3,13 +3,14 @@ package net.blay09.mods.forgivingvoid;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(ForgivingVoid.MOD_ID)
 public class NeoForgeForgivingVoid {
 
-    public NeoForgeForgivingVoid(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgeForgivingVoid(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         Balm.initializeMod(ForgivingVoid.MOD_ID, context, ForgivingVoid::initialize);
     }
 

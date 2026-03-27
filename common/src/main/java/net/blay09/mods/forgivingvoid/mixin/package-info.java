57cec79d53c7d1aeb793a240991b82e7c487c70a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-27 11:31:36 +0100

    refactor: Add package infos

---
+++ common/src/main/java/net/blay09/mods/forgivingvoid/mixin/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package net.blay09.mods.forgivingvoid.mixin;
+
+import org.jspecify.annotations.NullMarked;
---
+++ neoforge/src/main/java/net/blay09/mods/forgivingvoid/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package net.blay09.mods.forgivingvoid;
+
+import org.jspecify.annotations.NullMarked;

6fbe5415b772f01e2839bf5a2c3acbcdf28d2cc9
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-12 09:18:46 +0200

    feat!: Switch to our own Shogi scope for future proofing

--- common/src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoidRules.java
+++ common/src/main/java/net/blay09/mods/forgivingvoid/ForgivingVoidRules.java
@@ -4,12 +4,19 @@ import net.blay09.mods.shogi.Shogi;
 import net.blay09.mods.shogi.ShogiValue;
 import net.blay09.mods.shogi.context.MutableShogiContext;
 import net.blay09.mods.shogi.context.ShogiContext;
+import net.blay09.mods.shogi.scope.ShogiScope;
 import net.minecraft.world.entity.Entity;
+
+import java.util.List;
 
 import static net.blay09.mods.forgivingvoid.ForgivingVoid.id;
 
 public class ForgivingVoidRules {
-    public static final ShogiValue<ShogiContext, Integer> fallingHeight = Shogi.intValue(id("falling_height"), context -> context.requireLevel().getHeight());
+    public static final ShogiScope scope = Shogi.scope(id("rules"), it -> {
+        it.setDefaultNamespaces(List.of("forgivingvoid", "shogi"));
+    });
+
+    public static final ShogiValue<ShogiContext, Integer> fallingHeight = scope.intValue(id("falling_height"), context -> context.requireLevel().getHeight());
 
     public static ShogiContext makeContext(Entity entity) {
         return makeContext(entity, ForgivingVoid.getLoops(entity));
